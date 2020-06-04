c9d3ec1c83169e64cd8cf906575e78ce52774003
Shota Murano <dcmsd09072241242@gmail.com>
2020-06-04 10:55:04 +0900

    modify gettel() & HandyPhoneTest 継承の確認

--- src/Phone/HandyPhoneTest.java
+++ src/Phone/HandyPhoneTest.java
@@ -19,4 +19,10 @@ class HandyPhoneTest extends HandyPhone {
 	//		Assertions.assertEquals("smurano@gmail.com", hp1.getmail());
 	//	}
 
+	@Test
+	void test3() {
+		HandyPhone hp1 = new HandyPhone();
+		hp1.no = "55555";
+		Assertions.assertEquals("55555", hp1.gettel());
+	}
 }
--- src/Phone/Phone.java
+++ src/Phone/Phone.java
@@ -3,10 +3,9 @@ package Phone;
 class Phone {
 	String no;
 
-	//		String gettel(String no) {
-	//			this.no = no;
-	//			return no;
-	//		}
+	String gettel() {
+		return no;
+	}
 
 	void tel(String to) {
 		System.out.println(no + "から" + to + "へ電話しました");
